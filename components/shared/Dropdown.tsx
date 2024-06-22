254efff140e2e3d95a7b22100355780fe0ffdaf4
seyaendris <seide9894@gmail.com>
2024-06-22 22:59:46 +0300

    Creating Event and Category actions and also Uploadthing integrated for File Uploading

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { startTransition, useState } from 'react'
+import React, { startTransition, useEffect, useState } from 'react'
 import {
     Select,
     SelectContent,
@@ -20,6 +20,7 @@ import {
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
 import { Input } from '../ui/input'
+import { createCategory, getAllCategories } from '@/lib/actions/category.actions'
   
   
 
@@ -34,8 +35,23 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
     const [newCategory, setNewCategory] = useState('');
 
     const handleAddCategory = () => {
-      
+      createCategory({
+        categoryName: newCategory.trim()
+      })
+        .then((category) => {
+          setCategories((prevState) => [...prevState, category])
+        })
     }
+
+    useEffect(() => {
+      const getCategories = async () => {
+        const categoryList = await getAllCategories();
+  
+        categoryList && setCategories(categoryList as ICategory[])
+      }
+  
+      getCategories();
+    }, [])
   
   return (
 
@@ -51,7 +67,7 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
           ))}
 
         <AlertDialog>
-            <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-orange-800 hover:bg-orange-100 focus:text-orange-800">Open</AlertDialogTrigger>
+            <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-orange-800 hover:bg-orange-100 focus:text-orange-800">Add New Category</AlertDialogTrigger>
             <AlertDialogContent className="bg-white">
                 <AlertDialogHeader>
                 <AlertDialogTitle>New Category</AlertDialogTitle>
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -18,6 +18,9 @@ import Image from "next/image";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "@/components/ui/checkbox"
+import { useUploadThing } from '@/lib/uploadthing'
+import { useRouter } from "next/navigation";
+import { createEvent } from "@/lib/actions/event.actions";
 
 
 type EventFormProps = {
@@ -31,6 +34,10 @@ const EventForm = ({userId, type}: EventFormProps) => {
     const [files, setFiles] = useState<File[]>([])
     const initialValues = eventDefaultValues;
 
+    const router = useRouter();
+
+    const { startUpload } = useUploadThing('imageUploader');
+
 
     const form = useForm<z.infer<typeof EventFormSchema>>({
         resolver: zodResolver(EventFormSchema),
@@ -38,10 +45,35 @@ const EventForm = ({userId, type}: EventFormProps) => {
       })
      
       // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof EventFormSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+      async function onSubmit(values: z.infer<typeof EventFormSchema>) {
+        let uploadedImageUrl = values.imageUrl;
+
+        if(files.length > 0) {
+          const uploadedImages = await startUpload(files)
+    
+          if(!uploadedImages) {
+            return
+          }
+    
+          uploadedImageUrl = uploadedImages[0].url
+        }
+
+        if(type === 'Create') {
+          try {
+            const newEvent = await createEvent({
+              event: { ...values, imageUrl: uploadedImageUrl },
+              userId,
+              path: '/profile'
+            })
+    
+            if(newEvent) {
+              form.reset();
+              router.push(`/events/${newEvent._id}`)
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
       }
 
   return (
@@ -85,7 +117,7 @@ const EventForm = ({userId, type}: EventFormProps) => {
         render={({ field }) => (
           <FormItem className="w-full">
             {/* <FormLabel>Description</FormLabel> */}
-            <FormControl className="h-52">
+            <FormControl className="h-72">
               <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
             </FormControl>
             <FormMessage />
@@ -256,7 +288,7 @@ const EventForm = ({userId, type}: EventFormProps) => {
                         height={24}
                       />
 
-                      <Input placeholder="URL" {...field} className="input-field" />
+                      <Input placeholder="If it is Online provide a URL" {...field} className="input-field" />
                     </div>
 
                   </FormControl>
