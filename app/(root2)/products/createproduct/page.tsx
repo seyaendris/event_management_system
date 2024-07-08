5d7d961d4cee2c5ac48452c2e349146ec36a0a41
seyaendris <seide9894@gmail.com>
2024-07-08 16:40:01 +0300

    create productPage finished

--- app/(root2)/products/createproduct/page.tsx
+++ app/(root2)/products/createproduct/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import EventForm from "@/components/shared/EventForm"
+import ProductForm from "@/components/shared/ProductForm";
+import { auth } from "@clerk/nextjs/server";
 
 const CreateProduct = () => {
+    const {sessionClaims} = auth();
+    const userId = sessionClaims?.userId as string;
+
+
   return (
-    <div>CreateProduct</div>
+    <>
+    <section className="bg-orange-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Create Your Product</h3>
+    </section>
+
+    <div className="wrapper my-8 drop-shadow-md">
+      <ProductForm userId={userId} type="Create" />
+    </div>
+</>
   )
 }
 
