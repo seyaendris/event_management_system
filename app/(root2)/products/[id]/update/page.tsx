9bfc38ef08266e57d3a938510e57173be480b6e2
seyaendris <seide9894@gmail.com>
2024-07-08 15:36:03 +0300

    update the page in the id folder

---
+++ app/(root2)/products/[id]/update/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const UpdateProduct = () => {
+  return (
+    <div>
+      <h1>Update Product</h1>
+      {/* Your page content here */}
+    </div>
+  );
+};
+
+export default UpdateProduct;
