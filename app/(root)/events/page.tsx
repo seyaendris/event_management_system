4710b67fe4e2645e4b87fd68ec76ad75f80b54dd
Seid <seide9894@gmail.com>
2024-07-24 23:50:15 +0300

    created a home page for the dashboard

---
+++ app/(root)/events/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      <h1 className='mb-4 text-xl md:text-3xl font-semibold'>DashBoard</h1>
+    </main>
+  )
+}
+
+export default Home
