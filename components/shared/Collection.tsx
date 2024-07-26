d22186cfcdb639451eca36ae58df51aa1b524866
Seid <seide9894@gmail.com>
2024-07-26 22:35:10 +0300

    Collection component updated

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,8 +1,7 @@
-
-import { IEvent } from '@/lib/database/models/event.model'
-import React from 'react'
-import Pagination from './Pagination'
-import Card from './Card'
+import { IEvent } from '@/lib/database/models/event.model';
+import React from 'react';
+import Pagination from './Pagination';
+import Card from './Card';
 
 type CollectionProps = {
   data: IEvent[],
@@ -12,8 +11,9 @@ type CollectionProps = {
   page: number | string,
   totalPages?: number,
   urlParamName?: string,
-  collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
-}
+  collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events',
+  columns?: number, // Add this prop
+};
 
 const Collection = ({
   data,
@@ -23,12 +23,24 @@ const Collection = ({
   totalPages = 0,
   collectionType,
   urlParamName,
+  columns = 3, // Default to 3 columns
 }: CollectionProps) => {
+  const getColumnClasses = (columns: number) => {
+    switch (columns) {
+      case 2:
+        return 'sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2';
+      case 3:
+        return 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3';
+      default:
+        return 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3';
+    }
+  };
+
   return (
     <>
       {data.length > 0 ? (
         <div className="flex flex-col items-center gap-10">
-          <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10">
+          <ul className={`grid w-full grid-cols-1 gap-5 ${getColumnClasses(columns)}`}>
             {data.map((event) => {
               const hasOrderLink = collectionType === 'Events_Organized';
               const hidePrice = collectionType === 'My_Tickets';
@@ -37,7 +49,7 @@ const Collection = ({
                 <li key={event._id} className="flex justify-center">
                   <Card event={event} hasOrderLink={hasOrderLink} hidePrice={hidePrice} />
                 </li>
-              )
+              );
             })}
           </ul>
 
@@ -45,14 +57,14 @@ const Collection = ({
             <Pagination urlParamName={urlParamName} page={page} totalPages={totalPages} />
           )}
         </div>
-      ): (
+      ) : (
         <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
           <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
           <p className="p-regular-14">{emptyStateSubtext}</p>
         </div>
-      )} 
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Collection
+export default Collection;
