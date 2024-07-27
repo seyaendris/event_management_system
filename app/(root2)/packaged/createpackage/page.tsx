3b905dda94e7014a91118a017c8829a10d55ba60
Seid <seide9894@gmail.com>
2024-07-27 20:59:33 +0300

    updated the create package page

--- app/(root2)/packaged/createpackage/page.tsx
+++ app/(root2)/packaged/createpackage/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
 
-function CreatePackage() {
+import PackageForm from "@/components/shared/PackageForm";
+import { auth } from "@clerk/nextjs/server";
+
+const CreateProduct = () => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
   return (
-    <div>
-      Create Package
-    </div>
-  )
-}
+    <>
+      <section className="bg-orange-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Create Your Package</h3>
+      </section>
+      <div className="wrapper my-8 drop-shadow-md">
+        <PackageForm userId={userId} type="Create" />
+      </div>
+    </>
+  );
+};
 
-export default CreatePackage
+export default CreateProduct;
