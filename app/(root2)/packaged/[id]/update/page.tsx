6a3703f712b5a8cbe4b7196fbea1af7c7e0febfd
Seid <seide9894@gmail.com>
2024-07-28 22:14:39 +0300

    created a page to update the package

--- app/(root2)/packaged/[id]/update/page.tsx
+++ app/(root2)/packaged/[id]/update/page.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
 
-function UpdatePackage() {
+import PackageForm from "@/components/shared/PackageForm";
+import { getPackageById } from "@/lib/actions/package.actions";
+import { auth } from "@clerk/nextjs/server";
+
+type UpdatePackageProps = {
+  params: {
+    id: string
+  }
+}
+
+const UpdatePackage = async ({ params: { id } }: UpdatePackageProps) => {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+  const packages = await getPackageById(id)
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className="bg-orange-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Update Package</h3>
+      </section>
+
+      <div className="wrapper my-8 drop-shadow-lg">
+        <PackageForm 
+          type="Update" 
+          packages={packages} 
+          packageId={packages._id} 
+          userId={userId} 
+        />
+      </div>
+    </>
   )
 }
 
