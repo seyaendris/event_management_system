a297d4cd6d5bc0541669f5e0d1e275a671ae0af6
Seid <seide9894@gmail.com>
2024-07-28 22:15:51 +0300

    created a page to update the product

--- app/(root2)/products/[id]/update/page.tsx
+++ app/(root2)/products/[id]/update/page.tsx
@@ -1,13 +1,36 @@
 
-import React from 'react';
+import ProductForm from "@/components/shared/ProductForm";
+import { getProductById } from "@/lib/actions/product.actions";
+import { auth } from "@clerk/nextjs/server";
 
-const UpdateProduct = () => {
+type UpdateProductProps = {
+  params: {
+    id: string
+  }
+}
+
+const UpdateProduct = async ({ params: { id } }: UpdateProductProps) => {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+  const product = await getProductById(id)
+
   return (
-    <div>
-      <h1>Update Product</h1>
-      {/* Your page content here */}
-    </div>
-  );
-};
+    <>
+      <section className="bg-orange-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Update Product</h3>
+      </section>
 
-export default UpdateProduct;
+      <div className="wrapper my-8 drop-shadow-lg">
+        <ProductForm 
+          type="Update" 
+          product={product} 
+          productId={product._id} 
+          userId={userId} 
+        />
+      </div>
+    </>
+  )
+}
+
+export default UpdateProduct
