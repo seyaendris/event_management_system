d96baea696c22e969a2ff4af34d48c7fb7d3b178
Seid <seide9894@gmail.com>
2024-07-30 14:43:31 +0300

    update the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,15 +19,15 @@ export default async function Home({ searchParams }: SearchParamProps) {
   });
 
   return (
-    <>
-      <section className="relative bg-gradient-to-b from-orange-100 bg-contain py-5 md:py-10">
+    <div className='bg-neutral-50 h-full'>
+      <section className="relative bg-gradient-to-b from-orange-100 bg-contain py-5 md:py-10 h-svh">
         <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('/assets/images/hero3.jpg')" }}>
           <div className="absolute inset-0 bg-black opacity-60"></div> {/* Dark overlay */}
         </div>
-        <div className="relative wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0 text-white"> {/* Adjust text color to white */}
-          <div className="flex flex-col justify-center gap-8 z-10">
-            <h1 className="h1-bold text-orange-400">Host, Connect and Celebrate Your Event with Nuevents!</h1> {/* Adjust text color to white */}
-            <p className="p-regular-20 md:p-regular-24 text-gray-200">You can HOST your Event and connect with Customers, BOOK your favorite Event easily right here.</p> {/* Adjust text color to a lighter gray */}
+        <div className="relative wrapper grid grid-cols-1 gap-5 md:grid-cols-1 2xl:gap-0 text-white text-center"> {/* Adjust text color to white */}
+          <div className="flex flex-col justify-center gap-8 z-10 pt-5">
+            <h1 className="h1-bold text-orange-400 bg-clip-text text-transparent animate-pulse bg-gradient-to-r from-orange-600 to bg-orange-50">Host, Connect and Celebrate Your Event with Nuevents!</h1> {/* Adjust text color to white */}
+            <p className="p-regular-20 md:p-regular-24 text-gray-200  md:w-1/2 text-left pt-16">You can HOST your Event and connect with Customers, BOOK your favorite Event easily right here.</p> {/* Adjust text color to a lighter gray */}
             <Button size="lg" asChild className="button w-full sm:w-fit bg-orange-600 hover:bg-orange-500">
               <Link href="#events">
                 Explore Now
@@ -37,8 +37,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
         </div>
       </section> 
 
-      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12 bg-neutral-50">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold pt-3 mt-2">Trusted by Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
@@ -56,6 +56,6 @@ export default async function Home({ searchParams }: SearchParamProps) {
           columns={3} // Default columns for Home page
         />
       </section>
-    </>
+    </div>
   );
 }
