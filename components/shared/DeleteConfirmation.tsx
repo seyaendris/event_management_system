409dba9d6ef6d7d55f306169adaf0ba7b98a26cc
Seid <seide9894@gmail.com>
2024-07-30 14:51:50 +0300

    add somethings on the deleteconfirmation

--- components/shared/DeleteConfirmation.tsx
+++ components/shared/DeleteConfirmation.tsx
@@ -1,4 +1,3 @@
-
 'use client'
 
 import { useTransition } from 'react'
@@ -18,22 +17,36 @@ import {
 } from '@/components/ui/alert-dialog'
 
 import { deleteEvent } from '@/lib/actions/event.actions'
+import { deleteProduct } from '@/lib/actions/product.actions'
+import { deletePackage } from '@/lib/actions/package.actions'
 
-export const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
+export const DeleteConfirmation = ({ eventId, productId, packageId }: { eventId?: string, productId?: string, packageId?: string }) => {
   const pathname = usePathname()
   let [isPending, startTransition] = useTransition()
+
+  const handleDelete = async () => {
+    if (eventId) {
+      await deleteEvent({ eventId, path: pathname });
+    }
+    if (productId) {
+      await deleteProduct({ productId, path: pathname });
+    }
+    if (packageId) {
+      await deletePackage({ packageId, path: pathname });
+    }
+  };
 
   return (
     <AlertDialog>
       <AlertDialogTrigger>
-        <Image src="/assets/icons/delete.svg" alt="edit" width={20} height={20} />
+        <Image src="/assets/icons/delete.svg" alt="delete" width={20} height={20} />
       </AlertDialogTrigger>
 
       <AlertDialogContent className="bg-white">
         <AlertDialogHeader>
           <AlertDialogTitle>Are you sure you want to delete?</AlertDialogTitle>
           <AlertDialogDescription className="p-regular-16 text-grey-600">
-            This will permanently delete this event
+            This will be permanently deleted
           </AlertDialogDescription>
         </AlertDialogHeader>
 
@@ -41,10 +54,9 @@ export const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
           <AlertDialogCancel>Cancel</AlertDialogCancel>
 
           <AlertDialogAction
+            className='bg-orange-600 hover:bg-orange-500'
             onClick={() =>
-              startTransition(async () => {
-                await deleteEvent({ eventId, path: pathname })
-              })
+              startTransition(handleDelete)
             }>
             {isPending ? 'Deleting...' : 'Delete'}
           </AlertDialogAction>
