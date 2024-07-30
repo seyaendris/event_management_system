a496ebfe6ca468d5f2144822c71b42f5b0e28ffa
Seid <seide9894@gmail.com>
2024-07-30 14:55:24 +0300

    created productcollection

--- components/shared/ProductCollection.tsx
+++ components/shared/ProductCollection.tsx
@@ -49,9 +49,9 @@ const ProductCollection = ({
             ))}
           </ul>
 
-          {/* {totalPages > 1 && (
+          {totalPages > 1 && (
             <Pagination urlParamName={urlParamName} page={page} totalPages={totalPages} />
-          )} */}
+          )}
         </div>
       ) : (
         <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
